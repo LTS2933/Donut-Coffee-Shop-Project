1032900521138a00a4f2fd67de21fc3c9a9a9416
chrisosma24 <101012681+chrisosma24@users.noreply.github.com>
2023-04-14 15:32:04 -0400

    Create BasketActivity.java

---
+++ app/src/main/java/com/example/project5cs213/BasketActivity.java
@@ -0,0 +1,25 @@
+package com.example.project5cs213;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BasketActivity extends AppCompatActivity{
+
+    private Button backToMain;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+        this.backToMain = (Button) findViewById(R.id.mainBasket);
+        this.backToMain.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+
+}
