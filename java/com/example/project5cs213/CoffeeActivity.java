b6ee51d59fadf3e4adc159a39b0de6ceead97240
LTS2933 <116848955+LTS2933@users.noreply.github.com>
2023-04-25 12:45:26 -0400

    Add files via upload

---
+++ java/com/example/project5cs213/CoffeeActivity.java
@@ -0,0 +1,25 @@
+package com.example.project5cs213;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+    public class CoffeeActivity extends AppCompatActivity{
+
+        private Button backToMain;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.coffee_ordering_view);
+            this.backToMain = (Button) findViewById(R.id.mainCoffee);
+            this.backToMain.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    finish();
+                }
+            });
+        }
+
+    }
